0550ecc62fb9220430354fb18d3482e46a7c62e6
Lauw10 <deslauw10@gmail.com>
2025-03-20 09:28:10 -0400

    Create condition.js

--- condition.js
+++ condition.js
@@ -4,6 +4,7 @@ document.querySelector(".dl button").addEventListener("click", () => {
     var pass = document.querySelector(".pwd").value
   
        if (pass.trim() = "" && pass.length >= 6 && pass.length <= 20) {
+         alert("Bienvenue")
        }else {
          alert("Le mot de passe ne doit pas être vide")
        }
