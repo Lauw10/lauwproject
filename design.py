bf5f3d51185dd20d459945a9ec0745bf33eb9f26
Lauw10 <deslauw10@gmail.com>
2025-03-19 22:49:18 -0400

    ajout de l'index hml

---
+++ design.py
@@ -0,0 +1,13 @@
+
+from PIL import Image, ImageDraw, ImageFilter
+
+
+width, height = 500, 500
+img = Image.new("RGB", (width, height), "white")
+des = ImageDraw.Draw(img)
+
+#img.save("DL.png")
+
+print("number : ")
+
+
---
+++ python.py
@@ -0,0 +1,9 @@
+
+number = [42660758]
+
+commande = int(input("Entrez votre numéro : "))
+
+number.append(commande)
+
+
+print(number)
