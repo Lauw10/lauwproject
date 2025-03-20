6d4b3f6006be166a19062e90c46c2fc66f011056
Lauw10 <deslauw10@gmail.com>
2025-03-20 09:24:00 -0400

    Update condition.js

--- condition.js
+++ condition.js
@@ -1,3 +1,10 @@
 // DL
 
-alert("yes!")
+document.querySelector(".dl button").addEventListener("click", () => {
+    var pass = document.querySelector(".pwd").value
+  
+       if (pass.trim() = "" && pass.length >= 6 && pass.length <= 20) {
+       }else {
+         alert("Le mot de passe ne doit pas être vide")
+       }
+})
